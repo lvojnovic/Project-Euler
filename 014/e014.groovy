0e48b77f5c3af82dc934df7b71efbc9335a255e1
Lucija Vojnovic <lucija.vojnovic@gmail.com>
2012-05-26 21:32:17 +0200

    solution for problem 14

---
+++ 014/e014.groovy
@@ -0,0 +1,50 @@
+/**
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 
+10 terms. Although it has not been proved yet (Collatz Problem), 
+it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+Solution note:
+brute force is faster than the solution with a look-up map with already 
+calculated values...
+**/
+
+def getChainLength = { Double n ->
+    def br = 1
+    while (n != 1) {
+        if (n%2 == 0) { 
+            n=n/2
+        } else {
+            n=n*3+1
+        }
+        br++
+    }
+    br
+}
+
+
+def start = System.nanoTime()
+def maxLength = 0
+def maxInt = 1
+(1..1000000).each {
+    def length = getChainLength(it)
+    if (length > maxLength) {
+        maxLength = length
+        maxInt = it
+    }
+}
+
+def stop = System.nanoTime()
+println "Result: " + maxInt
+println "Time taken: " + (stop-start)/1e9
